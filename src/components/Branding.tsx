a71164ef2271bc99e79ba92aaabbca2758307972
happybirthdaynibbi <129252118+happybirthdaynibbi@users.noreply.github.com>
2024-03-18 21:38:42 +0530

    Update Branding.tsx

--- src/components/Branding.tsx
+++ src/components/Branding.tsx
@@ -1,22 +1,28 @@
 import { FC } from "react";
 
-const Branding: FC = ({ image, title, message }) => {
+interface BrandingProps {
+  image: string;
+  title: string;
+  message: string;
+}
+
+const Branding: FC<BrandingProps> = ({ image, title, message }) => {
   return (
-    <div class="ps-4 hidden py-4 lg:block">
-      <div class="relative h-full w-full overflow-hidden rounded-xl">
+    <div className="ps-4 hidden py-4 lg:block"> {/* Changed class to className */}
+      <div className="relative h-full w-full overflow-hidden rounded-xl"> {/* Changed class to className */}
         <img
           src={`assets/images/ai/${image}.jpg`}
           alt=""
-          class="h-full w-full -scale-x-100 transform"
+          className="h-full w-full -scale-x-100 transform" {/* Changed class to className */}
         />
-        <div class="bg-default-950/40 absolute inset-0">
-          <div class="flex h-full items-end justify-center">
-            <div class="text-start p-6">
-              <h5 class="mb-3 text-xl font-bold text-white">
+        <div className="bg-default-950/40 absolute inset-0"> {/* Changed class to className */}
+          <div className="flex h-full items-end justify-center"> {/* Changed class to className */}
+            <div className="text-start p-6"> {/* Changed class to className */}
+              <h5 className="mb-3 text-xl font-bold text-white">
                 SolDeployerAi<br />
                 {title}!
               </h5>
-              <p class="text-default-400 text-base font-medium">{message}</p>
+              <p className="text-default-400 text-base font-medium">{message}</p> {/* Changed class to className */}
             </div>
           </div>
         </div>
