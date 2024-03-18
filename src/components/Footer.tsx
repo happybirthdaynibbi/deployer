47784146ad526a926c08977c98aba78df799e699
happybirthdaynibbi <129252118+happybirthdaynibbi@users.noreply.github.com>
2024-03-18 21:48:48 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,11 @@
 import { FC } from "react";
-import { useForm, ValidationError } from "@formspree/react";
-import {
-  TiSocialFacebook,
-  TiSocialLinkedin,
-  TiSocialTwitter,
-  TiSocialYoutube,
-} from "react-icons/ti";
-import { AiOutlineCopyrightCircle } from "react-icons/ai";
-import { FaTelegram } from 'react-icons/fa';
+import { useForm } from "@formspree/react";
 
 export const Footer: FC = () => {
   const [state, handleSubmit] = useForm("mzbnzpqr");
   if (state.succeeded) {
     return (
-      <h1 class="md:text-5xl/tight my-4 max-w-lg text-4xl font-medium text-white">
+      <h1 className="md:text-5xl/tight my-4 max-w-lg text-4xl font-medium text-white">
         Thanks for sending your message!
       </h1>
     );
@@ -32,25 +24,15 @@ export const Footer: FC = () => {
     "Images & B-roll",
     "Site Map",
   ];
-  ///
+  
   return (
-    <footer class="bg-default-950/40 backdrop-blur-3xl">
-
-
-      <div class="border-t border-white/10 py-6">
-        <div class="md:text-start container flex h-full flex-wrap items-center justify-center gap-4 text-center md:justify-between lg:px-20">
-          <p class="text-default-400 text-base font-medium">
+    <footer className="bg-default-950/40 backdrop-blur-3xl">
+      <div className="border-t border-white/10 py-6">
+        <div className="md:text-start container flex h-full flex-wrap items-center justify-center gap-4 text-center md:justify-between lg:px-20">
+          <p className="text-default-400 text-base font-medium">
             @2024 SolDeployerAI All Rights Reserved.
-            {/* <a href="#">
-              Design & Crafted
-              <i
-                data-lucide="heart"
-                class="inline h-4 w-4 fill-red-500 text-red-500"
-              ></i>
-              by @theblockchaincoders
-            </a> */}
           </p>
-          <p class="text-default-400 text-base font-medium">
+          <p className="text-default-400 text-base font-medium">
             <a href="#">Terms Conditions & Policy</a>
           </p>
         </div>
